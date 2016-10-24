b24b1edea604f605bd9c491f390c506c14c8e067
xoiss <StreletsAA@gmail.com>
2016-10-24 21:27:54 +0300

    Phase code span is introduced instead of not sufficiently clear and internally contradicting Number of samples per period.

--- sinegen.c
+++ sinegen.c
@@ -2,54 +2,54 @@
 
 /**
  * \name    Integer data types for internal needs of DSP algorithms.
- * \details In the case when a DSP integer data type is represented with a wider data type of the simulator host
+ * \note    In the case when a DSP integer data type is represented with a wider data type of the simulator host
  *  platform, the higher order bits of such type are left unused. In the case of unsigned integer the unused bits are
- *  filled with zeroes; and in the case of signed integer the sign bit is propagated through them.
+ *  padded with zeroes; and in the case of signed integer the sign bit is propagated through them.
  */
 /**@{*/
-typedef int32_t     dsp_int22_t;        /*!< Signed integer, 22-bit. */
-typedef uint32_t    dsp_uint22_t;       /*!< Unsigned integer, 22-bit. */
+typedef int32_t     dsp_int22_t;        /*!< DSP signed integer, 22-bit. */
+typedef uint32_t    dsp_uint22_t;       /*!< DSP unsigned integer, 22-bit. */
 /**@}*/
 
 /**
- * \brief   Width of the extended integer data type used for internal needs of DSP algorithms, in bits.
+ * \brief   Width of the 22-bit integer data type used for internal needs of DSP algorithms, in bits.
  * \details DSP algorithms operate internally with 22-bit integer values which are used either as 22-bit unsigned
- *  integers or 22-bit signed integers with 21 bits used for value and the 22th bit used for the sign.
+ *  integers, or 22-bit signed integers with 21 bits used for value and the 22th bit used for the sign.
  */
-#define DSP_WIDE_INT_WIDTH      (22)
+#define DSP_INT22_WIDTH     (22)
 
 /**
  * \brief   Returns wave momentary phase increment (dPhi) per single sample depending on the generator frequency (Fo).
  * \param[in]   fo      Generator frequency (Fo), integer value expressed in units of 2^Ks Hz, where Ks is the sampling
  *  rate factor. Here Ks is integer and may be zero, or either positive, or negative. When Ks equals zero, Fo is
  *  actually expressed in whole Hertz.
- * \return  Phase increment, integer value expressed in units of 2*pi/Ns radians, where Ns is the number of samples per
+ * \return  Phase increment, integer value expressed in units of 2*pi/Ps radians, where Ps is the number of samples per
  *  single wave period.
  * \details The following formula is used:\n
  *  dPhi [radians/sample] = 2*pi * (Fo [Hz] / Fs [Hz]),\n
  *  where Fo is the generator frequency in Hz, Fs is the sample rate in Hz.
- * \details If converted to integer value expressed in units of 2*pi/Ns radians, the following formula takes place:\n
- *  dPhi [integer/sample] = dPhi [radians/sample] / 2*pi * Ns = (Fo [Hz] / Fs [Hz]) * Ns,\n
- *  where Ns is the number of samples per single wave period, and 2*pi radians stay for the single wave period.
- * \details As soon as Fs [Hz] = Ns * 2^Ks, where Ks is the sampling rate factor, the following takes place:\n
- *  dPhi [integer/sample] = Fo [Hz] / (Ns * 2^Ks) * Ns = Fo [Hz] / 2^Ks.
+ * \details If converted to integer value expressed in units of 2*pi/Ps radians, the following formula takes place:\n
+ *  dPhi [integer/sample] = dPhi [radians/sample] / 2*pi * Ps = (Fo [Hz] / Fs [Hz]) * Ps,\n
+ *  where Ps is the number of samples per single wave period, and 2*pi radians stay for the single wave period.
+ * \details As soon as Fs [Hz] = Ps * 2^Ks, where Ks is the sampling rate factor, the following takes place:\n
+ *  dPhi [integer/sample] = Fo [Hz] / (Ps * 2^Ks) * Ps = Fo [Hz] / 2^Ks.
  * \details And finally, if Fo is given as integer value expressed in units of 2^Ks Hz, the following holds:\n
  *  dPhi [integer/sample] = Fo [integer].
  * \note    Phase increment per single sample (dPhi), if evaluated as integer value expressed in units of
- *  2*pi/Ns radians, numerically equals to the generator frequency (Fo), if specified as integer value expressed in
- *  units of 2^Ks Hz; and it does not depend on the sampling rate (Fs), number of samples per single wave period (Ns),
+ *  2*pi/Ps radians, numerically equals to the generator frequency (Fo), if specified as integer value expressed in
+ *  units of 2^Ks Hz; and it does not depend on the sampling rate (Fs), number of samples per single wave period (Ps),
  *  and sampling rate factor (Ks).
  */
 #define PHASE_INC_PER_SAMPLE(fo)    (fo)
 
 /**
  * \brief   Returns value of sinus given a momentary phase.
- * \param[in]   phi     Momentary phase (Phi), integer value expressed in units of 2*pi/Ns radians, where Ns is the
+ * \param[in]   phi     Momentary phase (Phi), integer value expressed in units of 2*pi/Ps radians, where Ps is the
  *  number of samples per single wave period.
  * \return  Momentary amplitude of the function 2^(WI-1) * sin(Phi), integer value, where WI is the width of the
  *  extended integer data type used for DSP algorithms, in bits.
- * \details Valid values for \p Phi are from (-Ns/2) to (+Ns/2 - 1), where Ns is the number of samples per single wave
- *  period, which corresponds to the range from (-pi) to (+pi - 2*pi/Ns) radians; zero value corresponds to 0 radians.
+ * \details Valid values for \p Phi are from (-Ps/2) to (+Ps/2 - 1), where Ps is the number of samples per single wave
+ *  period, which corresponds to the range from (-pi) to (+pi - 2*pi/Ps) radians; zero value corresponds to 0 radians.
  * \details Function returns values in the range from -2^(WI-1) to +2^(WI-1), which corresponds to the range
  *  from -1 to +1; zero value corresponds to 0.
  */
@@ -59,18 +59,18 @@ static dsp_int22_t sini(const dsp_int16_t phi);
 
 /* Returns value of sinus given a momentary phase. */
 dsp_int22_t sini(const dsp_int16_t phi) {
-    assert((-SAMPLES_PER_PERIOD/2) <= phi && phi <= (+SAMPLES_PER_PERIOD/2 - 1));
-    return phi << (DSP_WIDE_INT_WIDTH - DSP_CONV_INT_WIDTH);    /* skeleton */
+    assert(PHASE_CODE_MIN <= phi && phi <= PHASE_CODE_MAX);
+    return phi << (DSP_INT22_WIDTH - DSP_INT16_WIDTH);      /* skeleton */
 }
 
 /* Returns generator momentary output for each single sample. */
 dsp_int16_t geni(const dsp_uint16_t fo) {
-    static dsp_int16_t phase = 0;       /* Current momentary phase, wrapped, in units of 2*pi/Ns radians. */
+    static dsp_int16_t phase = 0;       /* Current momentary phase, wrapped, in units of 2*pi/Ps radians. */
     const dsp_int16_t phi = phase;      /* Copy of the current momentary phase for postponed calculations. */
     const dsp_uint16_t dPhi = PHASE_INC_PER_SAMPLE(fo);     /* Phase increment. */
-    assert(0 < dPhi && dPhi <= SAMPLES_PER_PERIOD/2);       /* Equivalent to (0 < Fo and Fo <= Ns/2), where Ns == Fs. */
+    assert(0 < dPhi && dPhi <= PHASE_CODE_SPAN/2);       /* Equivalent to (0 < Fo and Fo <= Ps/2), where Ps == Fs. */
     phase += dPhi;
-    if (phase > (+SAMPLES_PER_PERIOD/2 - 1))
-        phase -= SAMPLES_PER_PERIOD;
-    return sini(phi) >> (DSP_WIDE_INT_WIDTH - DSP_CONV_INT_WIDTH);  /* skeleton */
+    if (phase > PHASE_CODE_MAX)
+        phase -= PHASE_CODE_SPAN;
+    return sini(phi) >> (DSP_INT22_WIDTH - DSP_INT16_WIDTH);    /* skeleton */
 }
--- sinegen.h
+++ sinegen.h
@@ -5,52 +5,79 @@
 
 /**
  * \name    Integer data types for external interface to DSP algorithms.
- * \details In the case when a DSP integer data type is represented with a wider data type of the simulator host
+ * \note    In the case when a DSP integer data type is represented with a wider data type of the simulator host
  *  platform, the higher order bits of such type are left unused. In the case of unsigned integer the unused bits are
- *  filled with zeroes; and in the case of signed integer the sign bit is propagated through them.
+ *  padded with zeroes; and in the case of signed integer the sign bit is propagated through them.
  */
 /**@{*/
-typedef int16_t     dsp_int16_t;        /*!< Signed integer, 16-bit. */
-typedef uint16_t    dsp_uint16_t;       /*!< Unsigned integer, 16-bit. */
+typedef int16_t     dsp_int16_t;        /*!< DSP signed integer, 16-bit. */
+typedef uint16_t    dsp_uint16_t;       /*!< DSP unsigned integer, 16-bit. */
 /**@}*/
 
 /**
- * \brief   Width of the conventional integer data type used for external interface to DSP algorithms, in bits.
+ * \brief   Width of the 16-bit integer data type used for external interface to DSP algorithms, in bits.
  * \details DSP algorithms operate externally with 16-bit integer values which are used either as 16-bit unsigned
- *  integers or 16-bit signed integers with 15 bits used for value and the 16th bit used for the sign.
+ *  integers, or 16-bit signed integers with 15 bits used for value and the 16th bit used for the sign.
  */
-#define DSP_CONV_INT_WIDTH      (16)
+#define DSP_INT16_WIDTH     (16)
 
 /**
- * \brief   Number of samples per single wave period (Ns).
- * \note    This value must be a power of two, and it must be in the range from 256 to 65536.
+ * \brief   Width of the integer code representing the momentary phase of the generator (Pw), in bits.
+ * \details The momentary phase of the generator wraps within the range [-pi; +pi) radians. The lower boundary is
+ *  included into, and the upper boundary is excluded from the range.
+ * \details For needs of DSP algorithms the momentary phase is represented as a signed integer value in the range
+ *  from -2^(Pw-1) to +2^(Pw-1)-1.
+ * \note    The value of Pw must be sufficiently high to achieve the appropriate accuracy, but it must not exceed the
+ *  width of the 16-bit integer data type used for external interface to DSP algorithms.
  */
-#define SAMPLES_PER_PERIOD      (65536)
-#if (256 > SAMPLES_PER_PERIOD || SAMPLES_PER_PERIOD > 65536)
-#   error   Number of samples per single wave period must be in the range from 256 to 65536.
+#define PHASE_CODE_WIDTH    (16)
+
+/* Validate the Pw value. */
+#if !(8 <= PHASE_CODE_WIDTH && PHASE_CODE_WIDTH <= DSP_INT16_WIDTH)
+#   error   Value of Pw is out of the valid range.
 #endif
-#if ((SAMPLES_PER_PERIOD & (SAMPLES_PER_PERIOD - 1)) != 0)
-#   error   Number of samples per single wave period must be a power of two.
-#endif
+
+/**
+ * \name    Set of derivative parameters of the integer code representing the momentary phase of the generator.
+ * \details For needs of DSP algorithms the momentary phase is represented as a signed integer value in the range
+ *  from -2^(Pw-1) to +2^(Pw-1)-1, where Pw is the width of the integer code representing the momentary phase.
+ * \details Special notation is introduced for this range: Pmin = -2^(Pw-1), Pmax = +2^(Pw-1)-1, Ps = 2^Pw. According to
+ *  these the momentary phase code belongs to the range from Pmin to Pmax inclusively and provides Ps different codes.
+ * \details The momentary phase code Pmin corresponds to -pi radians; code Pmax+1 corresponds to +pi radians; code Pmax
+ *  corresponds to (+pi - 2*pi/Ps) radians; code 0 corresponds to 0 radians.
+ * \details The phase code span Ps corresponds to one whole period 2*pi radian; the phase code single unit corresponds
+ *  to 2*pi/Ps radians.
+ * \details Generator increments the momentary phase code by one unit at each sampling tick when it is generating the
+ *  lower supported frequency Fo_min. For higher frequencies Fo = (Fo_min * k) the momentary phase code is incremented
+ *  by k units at each sampling tick (the same value k is used between all ticks).
+ */
+/**@{*/
+/** Span of the momentary phase integer code (Ps). */
+#define PHASE_CODE_SPAN     (1 << PHASE_CODE_WIDTH)
+/** Minimum value of the momentary phase integer code (Pmin). */
+#define PHASE_CODE_MIN      (-PHASE_CODE_SPAN/2)
+/** Maximum value of the momentary phase integer code (Pmax). */
+#define PHASE_CODE_MAX      (+PHASE_CODE_SPAN/2 - 1)
+/**@}*/
 
 /**
  * \brief   Sampling rate factor (Ks).
  * \details Sampling rate (Fs) is expressed in Hertz with the following formula:\n
- *  Fs [Hz] = Ns * 2^Ks,\n
- *  where Ns is the number of samples per single wave period, Ks is the sampling rate factor.
- * \note    If expressed not in whole Hertz but in units of 2^Ks Hz, the sampling rate (Fs) numerically equals Ns.
- * \details When Ks is set to zero (i.e., Fs numerically equals to Ns), it allows correct generation of frequencies
+ *  Fs [Hz] = Ps * 2^Ks,\n
+ *  where Ps is the number of samples per single wave period, Ks is the sampling rate factor.
+ * \note    If expressed not in whole Hertz but in units of 2^Ks Hz, the sampling rate (Fs) numerically equals Ps.
+ * \details When Ks is set to zero (i.e., Fs numerically equals to Ps), it allows correct generation of frequencies
  *  starting with 1 Hz and theoretically up to Fs/2 Hz with the step of 1 Hz.
- * \details When Ks is set to a positive value (i.e., Fs numerically equals to a two-in-a-power multiple of Ns), it
+ * \details When Ks is set to a positive value (i.e., Fs numerically equals to a two-in-a-power multiple of Ps), it
  *  allows correct generation of frequencies starting with 2^Ks Hz and theoretically up to Fs/2 Hz with the step of
  *  2^Ks Hz. This configuration extends the generator range to higher frequencies (as soon as Fs grows with 2^Ks), but
  *  reduces the resolution, both by 2^Ks.
- * \details When Ks is set to a negative value (i.e., Fs numerically equals to a two-in-a-power divider of Ns), it
+ * \details When Ks is set to a negative value (i.e., Fs numerically equals to a two-in-a-power divider of Ps), it
  *  allows correct generation of frequencies starting with 1/2^|Ks| Hz and theoretically up to Fs/2 Hz with the step of
  *  1/2^|Ks| Hz (notice that Ks is negative here). This configuration increases the resolution to fractions of Hertz,
  *  but shrinks the upper boundary of the generator range, both by 2^|Ks|.
  * \details When it is necessary to extend the generator range to higher frequencies but not to loose in the resolution
- *  the number of samples per single wave period (Ns) shall be increased.
+ *  the number of samples per single wave period (Ps) shall be increased.
  * \note    The generator frequency (Fo) is specified as an integer value expressed in units of 2^Ks Hz.
  */
 #define SAMPLING_RATE_FACTOR    (0)
@@ -73,11 +100,11 @@ typedef uint16_t    dsp_uint16_t;       /*!< Unsigned integer, 16-bit. */
 /**
  * \brief   Sampling rate (Fs), Hz.
  * \details Sampling rate (Fs) is expressed in Hertz with the following formula:\n
- *  Fs [Hz] = Ns * 2^Ks,\n
- *  where Ns is the number of samples per single wave period, Ks is the sampling rate factor.
+ *  Fs [Hz] = Ps * 2^Ks,\n
+ *  where Ps is the number of samples per single wave period, Ks is the sampling rate factor.
  * \note    The generator hardware shall be configured properly to actually achieve the declared sampling rate.
  */
-#define SAMLING_RATE_HZ     (MUL_2_POW_KS(SAMPLES_PER_PERIOD))
+#define SAMLING_RATE_HZ     (MUL_2_POW_KS(PHASE_CODE_SPAN))
 
 /**
  * \brief   Returns generator momentary output for each single sample.
