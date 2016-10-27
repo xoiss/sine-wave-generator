a8cbfef315b3dcbf51cde8f9f898796df19c07f9
xoiss <StreletsAA@gmail.com>
2016-10-27 19:18:31 +0300

    Phase-to-sine LUT generators are updated.

--- misc/phase-to-sine-lut.py
+++ misc/phase-to-sine-lut.py
@@ -4,11 +4,13 @@ values_per_line = 8
 
 from math import sin, pi
 
-templ = '0x{:0%uX}, ' % (value_width / 4)
+templ = '0x{:0%uX}, ' % round(value_width / 4 + .4)
 for i in range(0, keys_num, values_per_line):
     s = ''
     for j in range(values_per_line):
-        phi = (1 + (i + j) / keys_num) * pi / 2
-        sin_phi = round(sin(phi) * (2**value_width - 1))
+        phi = (i + j) / keys_num * pi / 2
+        sin_phi = round(sin(phi) * (2 ** value_width))
+        if sin_phi >= 2 ** value_width:
+            sin_phi = 2 ** value_width - 1
         s += templ.format(sin_phi)
     print(s)
