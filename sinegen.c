476c52d027f054249eb8719ac050dfe3f5c2325d
xoiss <StreletsAA@gmail.com>
2016-10-31 05:23:30 +0300

    Tiny update.

--- sinegen.c
+++ sinegen.c
@@ -15,47 +15,47 @@
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Initializes a sine wave generator. */
-void gen_init(struct gen_descr_t * const pdescr) {
-    assert(pdescr != NULL);
-    pdescr->freq = 0;
-    pdescr->phi = 0;
-    pdescr->att = 0;
+void gen_init(struct gen_descr_t * const pgen) {
+    assert(pgen != NULL);
+    pgen->freq = 0;
+    pgen->phi = 0;
+    pgen->att = 0;
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Assigns the generator frequency. */
-void gen_set_freq(struct gen_descr_t * const pdescr, const uq016_t freq) {
-    assert(pdescr != NULL);
+void gen_set_freq(struct gen_descr_t * const pgen, const uq016_t freq) {
+    assert(pgen != NULL);
     assert(freq <= 0x4000);
-    pdescr->freq = freq;
+    pgen->freq = freq;
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Assigns the generator phase. */
-void gen_set_phi(struct gen_descr_t * const pdescr, const uq016_t phi) {
-    assert(pdescr != NULL);
-    pdescr->phi = phi;
+void gen_set_phi(struct gen_descr_t * const pgen, const uq016_t phi) {
+    assert(pgen != NULL);
+    pgen->phi = phi;
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Assigns the generator output attenuation. */
-void gen_set_att(struct gen_descr_t * const pdescr, const uq016_t att) {
-    assert(pdescr != NULL);
-    pdescr->att = att;
+void gen_set_att(struct gen_descr_t * const pgen, const uq016_t att) {
+    assert(pgen != NULL);
+    pgen->att = att;
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Returns the generator momentary output. */
-sq015_t gen_output(const struct gen_descr_t * const pdescr) {
-    assert(pdescr != NULL);
-    return msin_sq015(pdescr->phi, pdescr->att);
+sq015_t gen_output(const struct gen_descr_t * const pgen) {
+    assert(pgen != NULL);
+    return msin_sq015(pgen->phi, pgen->att);
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Propagates the generator state for one sampling step. */
-void gen_step(struct gen_descr_t * const pdescr) {
-    assert(pdescr != NULL);
-    pdescr->phi += pdescr->freq;
+void gen_step(struct gen_descr_t * const pgen) {
+    assert(pgen != NULL);
+    pgen->phi += pgen->freq;
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
